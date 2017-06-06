de590cb134f4d4ea276b175d3a64976e8163e6c1
Stuart Davidson <stuartajd@users.noreply.github.com>
2017-06-06 17:58:50 +0100

    Spanish Translations

---
+++ es.php
@@ -0,0 +1,267 @@
+<?php 
+	/*
+		  Spanish TRANSLATIONS
+          TRUCKNET VERSION 1.6
+          © StuartD 2017 ( www.stuartd.co.uk )
+	*/
+	
+	$lang = array();
+
+	// GENERAL TRANSLATIONS
+	$lang['general_language_en_only'] = "Esta página sólo está disponible en español.";
+	$lang['general_language_info'] = "Información";
+	$lang['general_powered_by'] = "Powered by";
+	$lang['general_submit'] = "Enviar";
+	$lang['general_status_pending'] = "Pendiente";
+	$lang['general_status_denied'] = "Denegado";
+	$lang['general_status_accepted'] = "Aceptado";
+	$lang['general_unverified'] = "Usuario no verificado";
+	$lang['general_suspended'] = "Usuario Supendido";
+	$lang['general_pendingVerification'] = "verificación pendiente";
+	$lang['general_verified'] = "Usuario Verificado";	
+	$lang['general_admin'] = "Administrador";
+	$lang['general_status'] = "Estado del multijugador";
+	
+	// NAVIGATION BAR TRANSLATIONS
+	$lang['header_navbar_index'] = "Home";
+	$lang['header_navbar_blog'] = "Blog";
+	$lang['header_navbar_contact'] = "Contacto";
+	$lang['header_navbar_faq'] = "FAQ";
+	$lang['header_navbar_signin'] = "Inicio de Sesión";
+	$lang['header_navbar_register'] = "Registrarse";
+	$lang['header_navbar_about'] = "Acerca de";
+	
+	$lang['header_navbar_about_vtc'] = "";
+	$lang['header_navbar_badges'] = "Insignias";
+	
+	$lang['header_navbar_loggedin'] = "Conectado como";
+	$lang['header_navbar_messages'] = "Mensajes";
+	$lang['header_navbar_administration'] = "Administración";
+	
+	$lang['header_navbar_messages_view'] = "Ver mensjae";
+	$lang['header_navbar_messages_send'] = "Enviar mensaje";
+	
+	$lang['header_navbar_admin_announce'] = "Anuncios";
+	$lang['header_navbar_admin_blog'] = "Añadir publicación";
+	$lang['header_navbar_admin_trucking'] = "Añadir anuncio de trajectos";
+	
+	$lang['header_navbar_admin_user'] = "Información del usuario";
+	$lang['header_navbar_admin_all_user'] = "Todos los usuarios";
+	$lang['header_navbar_admin_pend_user'] = "Usuario Pendiente";
+	$lang['header_navbar_admin_user'] = "Información del usuario";
+	
+	$lang['header_navbar_admin_job'] = "Información del trabajo";
+	$lang['header_navbar_admin_all_job'] = "Todos los trabajos";
+	$lang['header_navbar_admin_pend_job'] = "Trabajo pendiente";
+	
+	$lang['header_navbar_admin_loc'] = "Información sobre la ubicación";
+	$lang['header_navbar_admin_all_loc'] = "Todas las ubicaciónes";
+	$lang['header_navbar_admin_add_loc'] = "Añadir ubicazión";
+	
+	$lang['header_navbar_dashboard'] = "Panel de control";
+	$lang['header_navbar_profile'] = "Perfil";
+	$lang['header_navbar_account_info'] = "Información de la cuenta";
+	$lang['header_navbar_logout'] = "Cerrar sesión";
+	$lang['header_navbar_submit'] = "Enviar un trabajo";
+	$lang['header_navbar_view_jobs'] = "Ver mis trabajos";
+	$lang['header_navbar_account_settings'] = "Configuraciones de la cuenta";
+	
+	$lang['header_navbar_settings'] = "Cofiguracion TruckNet";
+	$lang['header_navbar_settings_nav'] = "Editar configuracion TruckNet";
+	
+	$lang['header_navbar_settings_admin_settings'] = "Administracion de administradores";
+	$lang['header_navbar_settings_admin_badges'] = "Consulta de insignias";
+		
+	
+	// INDEX.PHP TRANSLATIONS
+
+	$Lang['index_lead_text'] = "Estamos liderando el camino hacia una entrega exitosa.";
+	$Lang['index_welcome_text_1'] = "Bienvenido al";
+	$Lang['index_welcome_text_2'] = "Sitio de TruckNet.";
+
+	$Lang['index_description_1'] = "Nos gustaría darle la bienvenida a nuestro sitio, un lugar donde todo se reúne.";
+	$Lang['index_description_2'] = "Mientras estás aquí, si no lo has hecho, ¿por qué no registrarte?";
+	$Lang['index_description_3'] = "Únete al grupo de los camioneros de élite que trabajan para entregar los bienes que nos necesitan también!";
+
+	$Lang['index_company_stats_text'] = "Estadísticas de nuestra empresa";
+	$Lang['index_company_stats_description'] = "Siempre estamos abiertos sobre el estado de la empresa, así que aquí están algunas de nuestras estadísticas de VTC.";
+
+	$Lang['index_total_money'] = "Dinero total levantado";
+	$Lang['index_total_drivers'] = "Total de controladores";
+	$Lang['index_total_jobs'] = "Total de trabajos";
+	$Lang['index_total_miles'] = "Total de millas impulsadas";
+	
+	
+	// CONTACTUS.PHP TRANSLATIONS
+	$Lang['contactus_title'] = "Directorio de personal";
+	$Lang['contactus_title_description'] = "Con el fin de obtener sus consultas ordenadas en la oportunidad más rápida, tenemos personal múltiple para hacer frente a diferentes tareas, por favor, elija la persona adecuada para su correo electrónico para obtener una respuesta más rápida.";
+
+	$Lang['table_name'] = "Nombre";
+	$Lang['table_message'] = "Mensaje";
+	$Lang['table_private'] = "Mensaje Privado";
+	
+	
+	// FAQ.PHP TRANSLATIONS
+	$Lang['faq_title'] = "Preguntas Frecuentes";
+	
+	$Lang['faq_what_is'] = "¿Qué es";
+	$Lang['faq_what_is_ans'] = "es una empresa de camiones virtuales construida por entusiastas de camiones que utilizan juegos como Euro Truck Simulator 2 y American Trucking Simulator.";
+
+	$Lang['faq_how_join'] = "¿Cómo puedo unirme?";
+	$Lang['faq_how_join_ans'] = "Simplemente registre su cuenta y usted puede comenzar a enviar sus unidades de inmediato!";
+
+	$Lang['faq_verified_what'] = "¿Cuál es la diferencia entre un usuario verificado y un usuario no verificado?";
+	$Lang['faq_verified_what_ans'] = "Los usuarios que no están verificados no pueden enviar un trabajo. Puede solicitar verificación en el panel de usuario.";
+
+	$Lang['faq_submit_how'] = "¿Cómo envío un trabajo?";
+	$Lang['faq_submit_how_ans'] = 'Una vez que te hayas registrado y accedido, haz clic en la opción "Iniciar sesión como:" Desplegable y selecciona "Enviar un trabajo". Una vez que hayas hecho clic en él, se te presentará un formulario Donde puede introducir la información sobre su viaje, luego presione Enviar. ';
+
+	$Lang['faq_avatar'] = "¿Cómo puedo cambiar mi avatar?";
+	$Lang['faq_avatar_ans'] = "Utilizamos un sistema llamado Gravatar para obtener tu avatar, si te registras con el mismo correo en gravatar.com tu avatar te mostrará!";
+	
+	$Lang['faq_accept_time'] = "¿Cuánto tiempo se tarda en aceptar un trabajo?";
+	$Lang['faq_accept_time_ans_1'] = "Los envíos de trabajos pueden tardar hasta 24 horas para ser actualizados, esto es completado por el personal de";
+	$Lang['faq_accept_time_ans_2'] = "para comprobar cada uno.";
+
+	$Lang['faq_denied'] = "¿Por qué se negó mi trabajo?";
+	$Lang['faq_denied_ans'] = "Podría haber habido algunas razones, éstas incluyen no ser exactas, equivocaciones o un beneficio demasiado bajo para la compañía, sólo negamos si hay una razón obvia que lo más probable es decir Usted en el mensaje privado. ";
+
+	$Lang['faq_trucknet'] = "Usted ha dicho Truck Net unas cuantas veces, ¿qué es Truck Net?";
+	$Lang['faq_trucknet_ans_1'] = "TruckNet es un CMS de PHP creado para gestionar una empresa de camiones virtuales creada por";
+	$Lang['faq_trucknet_ans_2'] = "es utilizado por";
+	$Lang['faq_trucknet_ans_3'] = "y está alimentando este sitio!";
+	
+	
+	// LOGIN.PHP TRANSLATIONS
+	$Lang['login_title'] = "Iniciar sesión";
+	$Lang['login_register_text'] = "Registro";
+	$Lang['login_failed'] = "Error de inicio de sesión";
+	$Lang['login_disabled'] = "Los inicios de sesión han sido desactivados por la administración";
+	$Lang['login_form_username'] = "Nombre de usuario";
+	$Lang['login_form_password'] = "Contraseña";
+	$Lang['login_forgot_password'] = "He olvidado mi contraseña";
+	
+	
+	// FORGOTPASSWORD.PHP TRANSLATIONS
+	$Lang['forgot_title'] = "Contraseña olvidada";
+	$Lang['forgot_description'] = "Si ha olvidado su contraseña actual a su cuenta, podemos restablecerla.";
+	$Lang['forgot_user'] = "Ingrese el nombre de usuario de la cuenta";
+	$Lang['forgot_email'] = "Ingrese el correo electrónico de la cuenta";
+	$Lang['forgot_submit'] = "Enviar";
+	$Lang['forgot_account_send'] = "Si hay una cuenta con ese usuario / correo electrónico, debe recibir un correo electrónico con información de restablecimiento.";
+	
+	
+	// FORGOTPASSWORDCHANGE.PHP TRANSLATIONS
+	$Lang['forgotChange_title'] = "Restablecer contraseña";
+	$Lang['forgotChange_description'] = "Utilice la clave que se envió por correo electrónico para restablecer su contraseña aquí.";
+	$Lang['forgotChange_user'] = "Introduzca el nombre de usuario de la cuenta";
+	$Lang['forgotChange_key'] = "Introduzca la tecla de reinicio";
+	$Lang['forgotChange_password'] = "Introduzca una nueva contraseña";
+	$Lang['forgotChange_success'] = "Su contraseña ha sido cambiada con éxito";
+	$Lang['forgotChange_error'] = "Algo salió mal, ¿están correctas la clave de reinicio y el nombre de usuario?";
+	
+	
+	// PERMISSION TRANSLATIONS
+	$Lang['permission_denied'] = "Permiso denegado";
+	$Lang['permission_desc'] = "No tiene permiso para acceder a esta página";
+	
+	
+	// ALLJOBS.PHP TRANSLATIONS
+	$Lang['allJobs_title'] = "Lista de trabajos";
+	$Lang['allJobs_driver'] = "Controlador";
+	$Lang['allJobs_start'] = "Inicio";
+	$Lang['allJobs_end'] = "End";
+	$Lang['allJobs_price'] = "Precio";
+	$Lang['allJobs_cost'] = "Costo";
+	$Lang['allJobs_status'] = "Estado";
+	
+	
+	// DASHBOARD.PHP TRANSLATIONS
+	$Lang['dashboard_status'] = "Estado";
+	$Lang['dashboard_signature'] = "Su firma en vivo";
+	$Lang['dashboard_welcome'] = "Bienvenido al panel de control";
+	$Lang['dashboard_requestVerify'] = "Solicitar verificación";
+	
+	
+	// REGISTER.PHP TRANSLATIONS
+	$Lang['register_title'] = "Formulario de registro";
+	$Lang['register_form_username'] = "Nombre de usuario";
+	$Lang['register_form_email'] = "Dirección de correo electrónico";
+	$Lang['register_form_password'] = "Contraseña";
+	$Lang['register_form_button'] = "Registrarme";
+
+	$Lang['register_error_enter_user'] = "Por favor ingrese un nombre de usuario";
+	$Lang['register_error_enter_pass'] = "Por favor, ingrese una contraseña";
+	$Lang['register_error_enter_email'] = "Por favor, ingrese una dirección de correo electrónico válida";
+	$Lang['register_error_captcha'] = "Captcha Entered Incorrectamente, vuelve y vuelve a intentarlo.";
+
+	$Lang['register_error_user'] = "El nombre de usuario ya está registrado";
+	$Lang['register_error_email'] = "Esta dirección de correo electrónico ya está registrada";
+
+	$Lang['location_page_title'] = "Enviar una ubicación";
+
+	
+	// EDITPROFILE.PHP TRANSLATIONS
+	$lang['editProfile_title'] = "Informacion del usuario";
+	$lang['editProfile_veri'] = "VERIFICADO";
+	$lang['editProfile_susp'] = "SUSPENDIDO";
+	$lang['editProfile_admi'] = "ADMIN";
+	$lang['editProfile_user'] = "Nombre de usuario";
+	$lang['editProfile_stat'] = "Rango";
+	$lang['editProfile_regD'] = "Fecha de Registro";
+	
+	$lang['editProfile_dInformation'] = "Información de conducción";
+	$lang['editProfile_dAccept'] = "Total de entregas aceptadas";
+	$lang['editProfile_dMiles'] = "Distancia total impulsada";
+	$lang['editProfile_dMoney'] = "Total de Dinero Ganado";
+	
+	$lang['editProfile_error'] = "Error!";
+	$lang['editProfile_desc'] = "¡Este perfil no existe, prueba otro!";
+	
+	
+	// SENDMESSAGE.PHP TRANSLATIONS
+	$lang['sendMess_title'] = "Enviar un mensaje privado";
+	$lang['sendMess_error'] = "¡Se ha producido un error! ¡Esa persona no tiene una cuenta con nosotros!";
+	$lang['sendMess_to'] = "Introduzca un nombre de usuario";
+	$lang['sendMess_subj'] = "Asunto del mensaje";
+	$lang['sendMess_mess'] = "Contenido del mensaje";
+	
+	
+	// MYJOBS.PHP TRANSLATIONS
+	$lang['myjobs_title'] = "Mis trabajos";
+	$lang['myjobs_desc'] = "Los trabajos tienen 3 estados. Pendiente - En espera de la aprobación de la Administración. Aceptado - Las estadísticas se agregan a su cuenta. Denegado - Esta ruta no estaba en su máximo potencial.";
+
+	
+	// PENDVERSUC.PHP TRANSLATIONS
+	$lang['verSuc_title'] = "Éxito!";
+	$lang['verSuc_desc_1'] = "Su cuenta se ha enviado para la verificación.";
+	$lang['verSuc_desc_2'] = "Esto tarda generalmente 24 - 48 horas, así que sea paciente.";
+	
+	
+	// SUBMITJOB.PHP TRANSLATIONS
+	$lang['submitJob_title'] = "Enviar trabajo";
+	$lang['submitJob_cannot_ver'] = "No puede enviar un trabajo hasta que sea un usuario verificado";
+	$lang['submitJob_cannot_image'] = "La imagen no es un archivo PNG o JPG";
+	$lang['submitJob_complete'] = "Su trabajo ya ha sido enviado, ha sido revisado manualmente por uno de los miembros del equipo. Este proceso puede tardar hasta 24 horas.";
+	
+	$lang['submitJob_form_gameMap'] = "Selecciona tu juego para poder seleccionar tu partida y destino.";
+	$lang['submitJob_form_game'] = "Selección de juegos";
+	$lang['submitJob_form_gameSel'] = "Por favor, seleccione el juego";
+	$lang['submitJob_form_user'] = "Nombre de usuario";
+	$lang['submitJob_form_startLoc'] = "Ubicación inicial";
+	$lang['submitJob_form_endLoc'] = "Ubicación de destino";
+	$lang['submitJob_form_distance'] = "Distancia";
+	$lang['submitJob_form_price'] = "Precio";
+	$lang['submitJob_form_cost'] = "Costo";
+	$lang['submitJob_form_fuelConsump'] = "El consumo de combustible";
+	$lang['submitJob_form_screenshot'] = "Captura de pantalla";
+	$lang['submitJob_form_uploadTool'] = "Herramienta de subida";
+	$lang['submitJob_form_imageLink'] = "Link para subir la imagen";
+	$lang['submitJob_form_comment'] = "Comentarios";
+	
+	// VIEWMESSAGES.PHP TRANSLATIONS
+	$lang['messages_title'] = "Mensajes privados";
+	$lang['messages_error_title'] = "Error";
+	$lang['messages_error_notexist'] = "Mensaje no existe!";
+	$lang['messages_error_notexist_desc'] = "¡Woh! El mensaje que acaba de intentar ver no existe, ¿qué pasó?";
+?>
